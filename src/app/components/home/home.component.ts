2af318688d66ed816e0af69c0a7f2708100b9b06
Tharun <tharunmurugadoss@gmail.com>
2025-11-27 18:53:17 +0530

    Tailwind css added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
